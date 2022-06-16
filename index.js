b47098b739755820aaf832fd650ddf632a18446d
cagucor <carlosfaguileracortes@gmail.com>
2022-06-16 14:01:45 +1000

    add colours

---
+++ index.js
@@ -0,0 +1,62 @@
+module.exports.themes = [
+  {
+    name: 'onenord-theme',
+    displayName: 'One Nord Theme ',
+    theme: {
+      background: {
+        default: '#2e3440',
+        success: '#a3be8c',
+        notice: '#e3a96c',
+        warning: '#d08f70',
+        danger: '#d57780',
+        surprise: '#b988b0',
+        info: '#88c0d0'
+      },
+      foreground: {
+        default: '#c8d0e0',
+        success: '#e5e9f0',
+        notice: '#e5e9f0',
+        warning: '#e5e9f0',
+        danger: '#e5e9f0',
+        surprise: '#e5e9f0',
+        info: '#e5e9f0'
+      },
+      highlight: {
+        default: 'rgba(114, 121, 133, 1)',
+        xxs: 'rgba(114, 121, 133, 0.05)',
+        xs: 'rgba(114, 121, 133, 0.1)',
+        sm: 'rgba(114, 121, 133, 0.2)',
+        md: 'rgba(114, 121, 133, 0.4)',
+        lg: 'rgba(114, 121, 133, 0.6)',
+        xl: 'rgba(114, 121, 133, 0.8)'
+      },
+      styles: {
+        sidebar: {
+          background: {
+            default: '#353b49'
+          }
+        },
+        dialog: {
+          background: {
+            default: '#303640'
+          }
+        },
+        paneHeader: {
+          background: {
+            success: '#a3be8c',
+            notice: '#e3a96c',
+            warning: '#d08f70',
+            danger: '#d57780',
+            surprise: '#b988b0',
+            info: '#88c0d0'
+          }
+        },
+        transparentOverlay: {
+          background: {
+            default: 'rgba(30, 33, 40, 0.8)'
+          }
+        }
+      }
+    }
+  }
+];
